99a467c1c2ffb94712eb39ef48b11248d6138333
Krishna-Suraj <suraj.1069@gmail.com>
2018-07-22 16:58:40 +0530

    updation

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -20,6 +20,7 @@ export class QuizComponent implements OnInit {
     this.httpClient.get('http://18.233.224.125:9090/questions/1')
       .subscribe((data) => {
         console.log(data.options[0].A);
+        this.question_id = data.questionId;
         this.question = data.question;
         this.options = [
           { value: data.options[0].A, opt: 'A' },
@@ -38,22 +39,22 @@ export class QuizComponent implements OnInit {
 
 
   nextQuestion() {
-    question_id++;
-    this.selected = null;
+    this.question_id++;
 
     this.httpClient.post('http://18.233.224.125:9090/user/responses',
       {
         'questionId': 1,
         'userId': 12,
         'testId': 111,
-        'selectedAnswer': B
+        'selectedAnswer': this.selected.opt
       }
     )
       .subscribe((data) => {
         console.log(data);
+        this.selected = null;
       });
 
-    this.httpClient.get('http://18.233.224.125:9090/questions/' + question_id)
+    this.httpClient.get('http://18.233.224.125:9090/questions/' + this.question_id)
       .subscribe((data) => {
         console.log(data.options[0].A);
         this.question = data.question;
